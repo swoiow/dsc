18c5a9ba4bbd6ca5e29022f812ce8f8d1890e959
swoiow <swoiow@users.noreply.github.com>
2018-09-04 22:42:48 +0800

    update install_docker

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 if [ `id -u` -eq 0 ];then
-    curl -fsSL get.docker.com -o get-docker.sh | bash
+    curl -fsSL get.docker.com | bash
     rm -f /usr/local/bin/docker-compose
     curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 else
-    sudo curl -fsSL get.docker.com -o get-docker.sh | bash
+    sudo curl -fsSL get.docker.com | bash
     sudo rm -f /usr/local/bin/docker-compose
     sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
