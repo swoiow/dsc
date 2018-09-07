092906aa01e4812a815d49dd6c46290aac407b99
Cell <swoiow@users.noreply.github.com>
2018-09-07 14:03:33 +0800

    Update __init__.py

--- scripts/fabic/__init__.py
+++ scripts/fabic/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import functools
 import platform
 import re
 
@@ -12,3 +13,19 @@ def pf():
         return "debian"
     elif re.search(re.compile("(centos|fedora)+"), v):
         return "redhat"
+
+
+def exec_bash(func):
+    from fabric.api import sudo
+
+    @functools.wraps(func)
+    def swap(*args, **kwargs):
+        command = func.__doc__
+        if command:
+            command = (c.strip() for c in command.split("\n") if c.strip())
+            for line in command:
+                sudo(line)
+
+        func(*args, **kwargs)
+
+    return swap
