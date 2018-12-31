7b30f9ed6a2af609c3fe27357c5bd790ed694d70
Swoiow <swoiow@users.noreply.github.com>
2018-12-31 15:05:33 +0800

    update aria2

---
+++ config-aria2/prepare.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -e
+
+UID=`id -u`
+GID=`id -g`
+
+echo
+echo "UID: $UID"
+echo "GID: $GID"
+echo
+
+echo "Setting conf"
+
+touch /aria2/config/aria2.session
+
+if [[ ! -e /aria2/config/aria2.conf ]]
+then
+  cp /etc/aria2.conf.default /aria2/config/aria2.conf
+fi
+
+echo "[DONE]"
+
+echo "Setting owner and permissions"
+
+chown -R $UID:$GID /aria2/config
+chmod 755 /aria2/config
+chmod 755 /aria2/downloads
+
+echo "[DONE]"
+
+echo "Starting aria2c"
+
+exec aria2c \
+    --conf-path=/aria2/config/aria2.conf \
+  > /dev/stdout \
+  2 > /dev/stderr
+
+echo 'Exiting aria2'
