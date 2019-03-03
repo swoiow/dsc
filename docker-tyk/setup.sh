171f4e143a4a81b969dbb6b55d6c098beb6d9c9c
Swoiow <swoiow@users.noreply.github.com>
2019-03-03 20:58:04 +0800

    update

--- docker-tyk/setup.sh
+++ docker-tyk/setup.sh
@@ -56,12 +56,12 @@ then
 fi
 
 echo "Creating Organisation"
-ORG_DATA=$(curl --silent --header "admin-auth: 12345" --header "Content-Type:application/json" --data '{"owner_name": "TykDev Ltd.","owner_slug": "dev", "cname_enabled":true, "cname": "'$TYK_PORTAL_DOMAIN'"}' http://$DOCKER_IP:3000/admin/organisations 2>&1)
+ORG_DATA=$(curl --silent --header "admin-auth: 612345" --header "Content-Type:application/json" --data '{"owner_name": "TykDev Ltd.","owner_slug": "dev", "cname_enabled":true, "cname": "'$TYK_PORTAL_DOMAIN'"}' http://$DOCKER_IP:3000/admin/organisations 2>&1)
 ORG_ID=$(echo $ORG_DATA | $PYTHON_BIN -c 'import json,sys;obj=json.load(sys.stdin);print(obj["Meta"])')
 echo "ORG ID: $ORG_ID"
 
 echo "Adding new user"
-USER_DATA=$(curl --silent --header "admin-auth: 12345" --header "Content-Type:application/json" --data '{"first_name": "Admin","last_name": "TYK","email_address": "'$TYK_DASHBOARD_USERNAME'","active": true,"org_id": "'$ORG_ID'"}' http://$DOCKER_IP:3000/admin/users 2>&1)
+USER_DATA=$(curl --silent --header "admin-auth: 612345" --header "Content-Type:application/json" --data '{"first_name": "Admin","last_name": "TYK","email_address": "'$TYK_DASHBOARD_USERNAME'","active": true,"org_id": "'$ORG_ID'"}' http://$DOCKER_IP:3000/admin/users 2>&1)
 USER_AUTH=$(echo $USER_DATA | $PYTHON_BIN -c 'import json,sys;obj=json.load(sys.stdin);print(obj["Message"])')
 USER_LIST=$(curl --silent --header "authorization: $USER_AUTH" http://$DOCKER_IP:3000/api/users 2>&1)
 USER_ID=$(echo $USER_LIST | $PYTHON_BIN -c 'import json,sys;obj=json.load(sys.stdin);print(obj["users"][0]["id"])')
@@ -80,7 +80,7 @@ echo "Creating the Portal Home page"
 OK=$(curl --silent --header "Authorization: $USER_AUTH" --header "Content-Type:application/json" --data '{"is_homepage": true, "template_name":"", "title":"Tyk Developer Portal", "slug":"home", "fields": {"JumboCTATitle": "Tyk Developer Portal", "SubHeading": "Sub Header", "JumboCTALink": "#cta", "JumboCTALinkTitle": "Your awesome APIs, hosted with Tyk!", "PanelOneContent": "Panel 1 content.", "PanelOneLink": "#panel1", "PanelOneLinkTitle": "Panel 1 Button", "PanelOneTitle": "Panel 1 Title", "PanelThereeContent": "", "PanelThreeContent": "Panel 3 content.", "PanelThreeLink": "#panel3", "PanelThreeLinkTitle": "Panel 3 Button", "PanelThreeTitle": "Panel 3 Title", "PanelTwoContent": "Panel 2 content.", "PanelTwoLink": "#panel2", "PanelTwoLinkTitle": "Panel 2 Button", "PanelTwoTitle": "Panel 2 Title"}}' http://$DOCKER_IP:3000/api/portal/pages 2>&1)
 
 echo "Fixing Portal URL"
-URL_DATA=$(curl --silent --header "admin-auth: 12345" --header "Content-Type:application/json" http://$DOCKER_IP:3000/admin/system/reload 2>&1)
+URL_DATA=$(curl --silent --header "admin-auth: 612345" --header "Content-Type:application/json" http://$DOCKER_IP:3000/admin/system/reload 2>&1)
 CAT_DATA=$(curl -X POST --silent --header "Authorization: $USER_AUTH" --header "Content-Type:application/json" --data "{}" http://$DOCKER_IP:3000/api/portal/configuration 2>&1)
 
 echo ""
