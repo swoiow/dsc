1d5ffd8a256068c4cac56be7aa3742202fed7748
Swoiow <swoiow@users.noreply.github.com>
2019-04-10 23:33:30 +0800

    add leetcode

---
+++ leetcode/py/addTwoNumbers.py
@@ -0,0 +1,26 @@
+class ListNode:
+
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        bucket = []
+        ctx = 0
+
+        while (l1 or l2 or ctx):
+            s = (l1 and l1.val or 0) + (l2 and l2.val or 0) + ctx
+            bucket.append(s % 10)
+            ctx = s >= 10 and 1 or 0
+
+            l1, l2 = l1 and l1.next or 0, l2 and l2.next or 0
+        return bucket
+
+
+if __name__ == '__main__':
+    o = Solution().addTwoNumbers([5], [5])
+
+    print(o)
---
+++ leetcode/py/twoSum.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from cells.wrapper import show_run_time
+
+
+class Solution:
+
+    @show_run_time
+    def twoSum(self, nums: list, target: int) -> (list, tuple):
+        hs = [(nums[0], 0)]
+
+        for ix, x1 in enumerate(nums[1:], start=1):
+            x2 = target - x1
+            if x2 in [i[0] for i in hs]:
+                return dict(hs)[x2], ix
+
+            if x1 not in hs:
+                hs.append((x1, ix))
+
+
+if __name__ == '__main__':
+    rest = Solution().twoSum()
+
+    print(rest)
