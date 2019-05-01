3f77c22b4b2e675d3969e237c25cc81ee84ecbfa
Swoiow <swoiow@users.noreply.github.com>
2019-05-01 21:00:23 +0800

    update

--- config-command/init.sh
+++ config-command/init.sh
@@ -5,3 +5,7 @@ cp ./.bashrc ~/.bashrc
 cp ./.zshrc ~/.zshrc
 cp ./.git-completion.bash ~/.git-completion.bash
 cat ./.bash_aliases > ~/.bash_aliases
+cd ../config-tmux
+sh init.sh
+cd ../config-vim
+cp ./.vimrc ~/.vimrc
